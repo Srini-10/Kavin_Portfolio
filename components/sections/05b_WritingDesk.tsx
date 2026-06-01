0c6e5df44fc18c46cf83a3f4d1608733a78db72e
Srinivasan P <srini.offcl@gmail.com>
2026-06-01 16:52:11 +0530

    Init: Initial Commit

--- components/sections/05b_WritingDesk.tsx
+++ components/sections/05b_WritingDesk.tsx
@@ -31,7 +31,7 @@ export default function WritingDesk() {
       </div>
 
       {/* Preview cards */}
-      <div className="mt-14 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-14 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
         {stories.map((story, i) => (
           <FadeIn key={story.id} delay={(i % 3) * 0.12}>
             <button
